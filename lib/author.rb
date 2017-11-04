e7bf4dbb16fa94734d6f9c68e3be436aa9a63274
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-04 14:08:43 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
 
   attr_accessor :name, :posts
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
@@ -10,6 +12,7 @@ class Author
   def add_post(post)
     post.author = self
     @posts << post
+    @@post_count += 1
   end
 
   def add_post_by_title(title)
@@ -17,5 +20,8 @@ class Author
     add_post(post)
   end
 
+  def self.post_count
+    @@post_count
+  end
 
 end
